3d8bdc5fdb208354a51c4a9c157a55f286fef9f3
KoenigOne <mm6dd.mh@gmail.com>
2025-10-04 19:19:46 +0100

    updated database connection code for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,8 @@
+const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./NAHRIMdb.db", (err) => {
+const dbPath = path.join(__dirname, "NAHRIMdb.db");
+const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Error opening database:", err.message);
   } else {
